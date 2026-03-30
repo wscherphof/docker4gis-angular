b18d9beed3b782bbbf70c223b98be5020f5c277b
Wouter <wouter.scherphof@gmail.com>
2026-03-30 14:20:26 +0200

    docs: instruct about `ng new`

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 angular_json=$(find . -name angular.json | head -n 1)
+
+if [ -z "$angular_json" ]; then
+	echo "No angular.json file found in this directory." >&2
+	echo "Create an Angular app first, then run the build again." >&2
+	exit 1
+fi
+
 SRC=$(dirname "$angular_json")
 
 docker image build \
