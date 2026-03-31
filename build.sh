57d32049e6ffcb19efae6ac63e81826bb6f42e4f
Wouter <wouter.scherphof@gmail.com>
2026-03-31 10:28:34 +0200

    fix: ng new
    Must find angular.json again afterwards.

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-angular_json=$(find . -name angular.json | head -n 1)
+function src() {
+	angular_json=$(find . -name angular.json | head -n 1)
+	SRC=$(dirname "$angular_json")
+	[ -n "$SRC" ]
+}
 
 # If we're building not the base component, but an extension image, and there's
 # no angular.json, then we create a new Angular project.
-if [ -z "$angular_json" ] && ! [ "${DOCKER_USER:-}" = "docker4gis" ]; then
+if ! [ "${DOCKER_USER:?}" = "docker4gis" ] && ! src; then
 	which ng || npm install -g @angular/cli &&
-		ng new "$DOCKER_USER-app" --skip-git true || exit
+		ng new "$DOCKER_USER-app" --skip-git true &&
+		src || exit
 fi
-
-SRC=$(dirname "$angular_json")
 
 docker image build \
 	--build-arg DOCKER_USER="$DOCKER_USER" \
