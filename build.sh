d32ec699eef0e60ad887ba906786944b1398f96e
Wouter <wouter.scherphof@gmail.com>
2026-04-01 11:45:59 +0200

    feat: add configuration files and enhance build script for Angular app

--- build.sh
+++ build.sh
@@ -6,12 +6,24 @@ function src() {
 	[ -n "$angular_json" ]
 }
 
+here=$(dirname "$0")
+
 # If we're building not the base component, but an extension image, and there's
 # no angular.json, then we create a new Angular project.
 if ! [ "${DOCKER_USER:?}" = "docker4gis" ] && ! src; then
+	app="$DOCKER_USER-app"
 	which ng || npm install -g @angular/cli &&
-		ng new "$DOCKER_USER-app" --skip-git true &&
-		src || exit
+		# Create a new Angular project, skipping git initialization.
+		ng new "$app" --skip-git true &&
+		src || exit &&
+
+		# https://angular.dev/ai/develop-with-ai#rules-files
+		cp -r "$here/conf/app/." "$app" &&
+
+		# https://angular.dev/ai/agent-skills
+		pushd "$app" &&
+		npx skills add https://github.com/angular/skills --skill '*' --yes &&
+		popd || exit
 fi
 
 docker image build \
